f43e817b3f12edcfe304fa175fb6af96c53f8e0c
XIMENES Carlos Ximenes (parceiro) <CXXIMENES@parceiro.mc.pt>
2024-05-21 22:04:11 -0300

    Aula 03

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { EquipmentType } from 'src/app/types/equipment.type';
 import { HomeService } from './home.service';
+import { EquipmentFilterType } from 'src/app/types/equipment-filter.type';
+import { EquipmentService } from 'src/app/services/equipment.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -9,17 +12,30 @@ import { HomeService } from './home.service';
 export class HomeComponent {
   public listaEquipamentos: EquipmentType[] = [];
 
-  constructor(private _homeService: HomeService) {
+  constructor(
+    private _equipmentService: EquipmentService,
+    private _router: Router
+  ) {
     this.filtrarEquipamentosPorNome();
   }
 
-  public filtrarEquipamentosPorNome(nome: string = '') {
-    this._homeService
-      .getListaEquipamento(nome)
-      .subscribe((resp) => (this.listaEquipamentos = resp));
+  public filtrarEquipamentosPorNome(filtro?: EquipmentFilterType) {
+    this._equipmentService.getListaEquipamento(filtro).subscribe({
+      next: (resp) => {
+        this.listaEquipamentos = resp;
+        console.log('next');
+      },
+      error: (err) => {
+        console.log(err);
+        console.log('error');
+      },
+      complete: () => {
+        console.log('complete');
+      },
+    });
   }
 
   public getDetalhesEquipamento(id: string) {
-    this._homeService.getEquipamento(id).subscribe((resp) => console.log(resp));
+    this._router.navigate([`/details/${id}`]);
   }
 }
--- src/app/pages/home/home.service.ts
+++ src/app/pages/home/home.service.ts
@@ -1,28 +1,6 @@
 import { Injectable } from '@angular/core';
-import { EquipmentListMock } from 'src/app/mocks/equipment-list.mock';
-import { EquipmentType } from 'src/app/types/equipment.type';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HomeService {
-  private _URL = 'https://mandalorian-store.netlify.app/api';
-
-  constructor(private _http: HttpClient) {}
-
-  public getListaEquipamento(
-    filtroNome: string = ''
-  ): Observable<EquipmentType[]> {
-    const nome = filtroNome.trim();
-    const options = nome
-      ? { params: new HttpParams().set('search', nome) }
-      : {};
-    return this._http.get<EquipmentType[]>(`${this._URL}/equipments`, options);
-  }
-
-  public getEquipamento(id: string): Observable<EquipmentType> {
-    return this._http.get<EquipmentType>(`${this._URL}/equipments/${id}`);
-  }
-}
+export class HomeService {}
